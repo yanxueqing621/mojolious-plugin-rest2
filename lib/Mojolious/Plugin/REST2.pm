a0041bf8ca98c0380f63c32fd2e95f1167710a28
Yan Xueqing <yanxueqing621@163.com>
2015-07-26 23:38:22 +0800

    Modified dist.ini file

--- lib/Mojolious/Plugin/REST2.pm
+++ lib/Mojolious/Plugin/REST2.pm
@@ -1,5 +1,8 @@
 package Mojolious::Plugin::REST2;
 use Modern::Perl;
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::Exception;
+use Lingua::EN::Inflect 1.895 qw/PL/;
 
 # VERSION
 # ABSTRACT: Mojolious::Plugin::REST2
@@ -13,6 +16,34 @@ use Modern::Perl;
 =head1 DESCRIPTION
 
 =cut
+
+my $http2crud = {
+    collection => {
+        get  => 'list',
+        post => 'create',
+    },
+    resource => {
+        get    => 'read',
+        put    => 'update',
+        delete => 'delete'
+    },
+};
+ 
+has install_hook => 1;
+
+
+sub register {
+  my $self = shift;
+  my $app = shift;
+  my $options = {@_ ? (ref $_[0] ? %{ $_[0] : @_) : ()};
+
+
+
+}
+
+
+
+
 
 
 1;
