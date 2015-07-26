01e2a623c7a820b8b112864c2dec1002609022d4
Yan Xueqing <yanxueqing621@163.com>
2015-07-26 22:45:47 +0800

    Initialize gitrepo with 'dzil new' command

---
+++ lib/Mojolious/Plugin/REST2.pm
@@ -0,0 +1,18 @@
+package Mojolious::Plugin::REST2;
+use Modern::Perl;
+
+# VERSION
+# ABSTRACT: Mojolious::Plugin::REST2
+
+=head1 SYNOPSIS
+
+  use Mojolious::Plugin::REST2
+  ..
+
+
+=head1 DESCRIPTION
+
+=cut
+
+
+1;
---
+++ t/90-mojolious-plugin-rest2.t
@@ -0,0 +1,7 @@
+use Modern::Perl;
+use Test::More;
+my $module = 'Mojolious::Plugin::REST2';
+use_ok($module);
+
+done_testing;
+
